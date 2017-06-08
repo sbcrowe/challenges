b6f36b4e0c64cb464cc225e8301e835b49810cc9
Scott Crowe <sb.crowe@gmail.com>
2017-06-08 20:59:05 +1000

    Add Project Euler solutions

--- projecteuler/problems_1.py
+++ projecteuler/problems_1.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Solutions to Project Euler problems 1-10, available at https://projecteuler.net/archives"""
+""" Solutions to Project Euler problems 1-50, available at https://projecteuler.net/archives """
 
 # authorship information
 __author__ = 'Scott Crowe'
@@ -10,7 +10,7 @@ __license__ = 'MIT'
 import math
 
 
-def sum_multiples_of_3_or_5(limit):
+def problem_01_multiples_3_5(limit):
     """ Problem 1: Calculates the sum of all multiples of 3 or 5 below limit.
 
     Args:
@@ -23,7 +23,7 @@ def sum_multiples_of_3_or_5(limit):
     return multiple_sum
 
 
-def even_fibonacci_sum(limit):
+def problem_02_fibonacci_sum(limit):
     """ Problem 2: Calculates the sum of all even terms in Fibonacci sequence below limit.
 
     Args:
@@ -39,7 +39,7 @@ def even_fibonacci_sum(limit):
     return sum_even_terms
 
 
-def largest_prime_factor(number):
+def problem_03_prime_factor(number):
     """ Problem 3: Determines the largest prime factor for a number.
 
     Args:
@@ -61,7 +61,7 @@ def largest_prime_factor(number):
     return max(prime_factors(number))
 
 
-def largest_palindrome_product(digits):
+def problem_04_palindrome_product(digits):
     """ Problem 4: Determines the largest palindrome product of two numbers with set number of digits.
 
     Args:
@@ -82,7 +82,7 @@ def largest_palindrome_product(digits):
                     result = product
 
 
-def smallest_divisible_by_set(set):
+def project_05_smallest_divisible(set):
     """ Problem 5: Determine the smallest number divisible by all numbers in the set.
 
     Args:
@@ -110,7 +110,7 @@ def smallest_divisible_by_set(set):
     return product
 
 
-def sum_square_difference(count):
+def project_06_sum_square_difference(count):
     """ Problem 6: Determines the difference between the sum of squares and square of sum of defined count of numbers.
 
     Args:
@@ -124,7 +124,7 @@ def sum_square_difference(count):
     return square_sum - sum_squares
 
 
-def prime_number(index):
+def project_07_prime_number(index):
     """ Problem 7: Determines the nth prime number.
 
     Args:
@@ -144,7 +144,7 @@ def prime_number(index):
     return primes[-1]
 
 
-def largest_product_in_series(count):
+def project_08_largest_product(count):
     """ Problem 8: Find the largest product of n numbers in a hardcoded series..
 
     Args:
@@ -189,7 +189,7 @@ def largest_product_in_series(count):
     return max_product
 
 
-def pythagorean_triplet(summation):
+def project_09_pythagorean_triplet(summation):
     """ Problem 9: Find the product of pythagorean triplet a, b, c, that sums to user specified total.
 
     Args:
@@ -204,7 +204,7 @@ def pythagorean_triplet(summation):
     return None
 
 
-def sum_primes(limit):
+def problem_10_sum_primes(limit):
     """ Problem 10: Sum all primes below limit.
 
     Args:
@@ -217,14 +217,66 @@ def sum_primes(limit):
             sieve[int(factor * multiple)] = 0
     return sum(sieve)
 
+
+def problem_25_fibonacci_digit_length(digits):
+    """ Problem 25: Find the index of the first term in the Fibonacci sequence to contain specified number of digits.
+
+    Args:
+        digits (int): The number of digits.
+    """
+    curr_index = 3
+    curr_term = 2
+    prev_term = 1
+    prev_prev_term = 1
+    while len(str(curr_term)) < digits:
+        prev_prev_term = prev_term
+        prev_term = curr_term
+        curr_term = prev_prev_term + prev_term
+        curr_index += 1
+    return curr_index
+
+
+def problem_30_narcissistic_number(power):
+    """ Problem 30: Find sum of numbers that can be written as the sum of powers of their digits.
+
+    Args:
+        power (int): The exponent of the digits.
+    """
+    # find limit to search
+    limit = len(str(9**power))*(9**power)
+    valid_numbers = []
+    for number in range(2, limit, 1):
+        sum_of_powers_of_digits = 0
+        for char in str(number):
+            sum_of_powers_of_digits += int(char) ** power
+        if sum_of_powers_of_digits == number:
+            valid_numbers.append(number)
+    return sum(valid_numbers)
+
+
+def problem_48_self_powers(series_limit, digits):
+    """ Problem 48: Find the last digits of series n^n (n = 1 to series limit).
+
+    Args:
+        series_limit (int): The maximum base and power in the series.
+        digits (int): The number of last digits to take from sum.
+    """
+    result = 0
+    for number in range(1, series_limit + 1, 1):
+        result += number ** number
+    return str(result)[-digits:]
+
 # run problems
-print("Problem 1 - Sum of all multiples of 3 or 5 below 1000: " + str(sum_multiples_of_3_or_5(1000)))
-print("Problem 2 - Sum of even valued terms in Fibonacci sequence < 4 million: " + str(even_fibonacci_sum(4000000)))
-print("Problem 3 - Largest prime factor of 600851475143: " + str(largest_prime_factor(600851475143)))
-print("Problem 4 - Largest palindrome product of two 3 digit numbers: " + str(largest_palindrome_product(3)))
-print("Problem 5 - Smallest number divisible by 1 to 20: " + str(smallest_divisible_by_set(range(1, 21, 1))))
-print("Problem 6 - Difference between sum of squares and square of sums (1-10): " + str(sum_square_difference(100)))
-print("Problem 7 - Determine the 10001st prime number: " + str(prime_number(10001)))
-print("Problem 8 - Determine largest product in series: " + str(largest_product_in_series(13)))
-print("Problem 9 - Find pythagorean triplet where a + b + c = 1000: " + str(pythagorean_triplet(1000)))
-print("Problem 10 - Find the sum of all primes < 2,000,000: " + str(sum_primes(2000000)))
+print("Problem 1 - Sum of all multiples of 3 or 5 below 1000: " + str(problem_01_multiples_3_5(1000)))
+print("Problem 2 - Sum of even valued terms in Fibonacci sequence < 4 million: " + str(problem_02_fibonacci_sum(4000000)))
+print("Problem 3 - Largest prime factor of 600851475143: " + str(problem_03_prime_factor(600851475143)))
+print("Problem 4 - Largest palindrome product of two 3 digit numbers: " + str(problem_04_palindrome_product(3)))
+print("Problem 5 - Smallest number divisible by 1 to 20: " + str(project_05_smallest_divisible(range(1, 21, 1))))
+print("Problem 6 - Difference between sum of squares and square of sums (1-10): " + str(project_06_sum_square_difference(100)))
+print("Problem 7 - Determine the 10001st prime number: " + str(project_07_prime_number(10001)))
+print("Problem 8 - Determine largest product in series: " + str(project_08_largest_product(13)))
+print("Problem 9 - Find pythagorean triplet where a + b + c = 1000: " + str(project_09_pythagorean_triplet(1000)))
+print("Problem 10 - Sum of all primes < 2,000,000: " + str(problem_10_sum_primes(2000000)))
+print("Problem 25 - Index of first Fibonacci term containing 1000 digits: " + str(problem_25_fibonacci_digit_length(1000)))
+print("Problem 30 - Sum of all numbers the sum of the fifth powers of their digits: " + str(problem_30_narcissistic_number(5)))
+print("Problem 48 - Last 10 digits of sum of series n^n, from 1 to 1000: " + problem_48_self_powers(1000, 10))
